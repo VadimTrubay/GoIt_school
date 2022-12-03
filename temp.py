7c8ea7cedbf71cd3646c9f48c1e82d70ac23f8ec
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-03 14:00:31 +0200

    main

--- temp.py
+++ temp.py
@@ -1,6 +1,10 @@
-
-a = int(input("enter: "))
-if a >= 10:
-    print('yes')
-else:
-    print('no')
+is_active = bool(input("Is the user active? "))
+is_admin = bool(input("Is the user administrator? "))
+is_permission = bool(input("Does the user have access? "))
+access = is_permission
+if is_permission is True or False and is_active is True or False:
+    access = True
+print(access)
+if is_permission is True and is_active is True:
+    access = True
+print(access)
