142ea21b3d68164f35bbf807c283ca01941b8da5
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-13 10:57:34 +0200

    main

---
+++ pathlib.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        user_input = ''
+    else:
+        user_input = sys.argv[1]
+    path = Path(user_input)
+    if path.exists():
+        if path.is_dir():
+            items = path.iterdir()
+            for item in items:
+                print(item)
+        else:
+            print(f'{path} is a file')
+    else:
+        print(f'{path.absolute()} is not a directory')
+
+if __name__ == "__main__":
+    main()
