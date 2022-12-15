257881b06c36b7e0021ec6d20f0d38d97cc4df07
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-15 10:10:42 +0200

    main

--- recurs/recurs.py
+++ recurs/recurs.py
@@ -2,14 +2,14 @@ import sys
 import pathlib
 
 
-def recursive_print(path, depth=0, margin_sumbol='-'):
-    margin = margin_sumbol + depth
+def recursive_print(path, depth=0, margin_symbol='-'):
+    margin = margin_symbol * depth
     if path.is_dir():
         print(margin + path.name + '/')
         for item in path.iterdir():
-            recursive_print(item, depth=depth+1)
+            recursive_print(item, depth+1)
     else:
-        print(margin + path.name)
+        print(path.name)
 
 
 def main():
@@ -17,5 +17,5 @@ def main():
     recursive_print(path)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
