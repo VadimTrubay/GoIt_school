5a320e8d6fb0a2e90a05838ca5ee2bf0ad2bdc0b
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-15 09:49:49 +0200

    main

--- pat_func/pat_func.py
+++ pat_func/pat_func.py
@@ -2,9 +2,21 @@ import pathlib
 import sys
 
 
+def print_file_name(file_name, full_name, margin, margin_item):
+    margin = margin * margin_item
+    if full_name:
+        print(margin + str(file_name.absolute()))
+    else:
+        print(margin + str(file_name.name))
+
+
 def main():
-    file_name = pathlib.Path(sys.argv[0])
-    print(file_name.name)
+    file_name = pathlib.Path(sys.argv[1])
+    print_file_name(file_name=file_name,
+                    full_name=False,
+                    margin=0,
+                    margin_item='-')
+    # print(file_name.absolute())
 
 
 if __name__ == '__main__':
--- pathlib/pathlib.py
+++ pathlib/pathlib.py
@@ -10,16 +10,15 @@ def main():
     path = Path(user_input)
     if path.exists():
         if path.is_dir():
-            items = path.iterdir()
+            # items = path.iterdir()
+            # items = path.glob('*.py')
+            items = path.glob('**/*')
             for item in items:
                 print(item)
         else:
             print(f'{path} is a file')
     else:
         print(f'{path.absolute()} is a not exists')
-
-
-
 
 
 if __name__ == "__main__":
