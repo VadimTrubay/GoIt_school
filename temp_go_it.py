b5fa1a5f4095c9fff0cf703924a1e635527e0f2f
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-17 22:39:37 +0200

    main

--- temp_go_it.py
+++ temp_go_it.py
@@ -881,3 +881,24 @@
 # print(a)
 # print(sys.getsizeof(l))
 # print(sys.getsizeof(t))
+
+a = {'F': 1,
+     'FX': 2,
+     'E': 3,
+     'D': 3,
+     'C': 4,
+     'B': 5,
+     'A': 5}
+def get_grade(key):
+    return a.get(key)
+
+
+b = {'F': 'Unsatisfactorily',
+     'FX': 'Unsatisfactorily',
+     'E': 'Enough',
+     'D': 'Satisfactorily',
+     'C': 'Good',
+     'B': 'Very good',
+     'A': 'Perfectly'}
+def get_description(key):
+    return b.get(key)
