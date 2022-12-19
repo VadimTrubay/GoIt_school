209fb3463e49de466c36b204a3f025fb3fe2476b
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-19 15:22:50 +0200

    main

--- counter_char.py
+++ counter_char.py
@@ -1,10 +1,11 @@
 def counter_char(string):
     dict_counter = {}
     for char in string:
-        try:
-            count = dict_counter[char]
-        except KeyError:
-            count = 0
+        # try:
+        #     count = dict_counter[char]  # получаем значение по ключу
+        # except KeyError:
+        #     count = 0
+        count = dict_counter.get(char, 0)  # получаем значение по ключу
         count += 1
-        dict_counter[char] = count
+        dict_counter[char] = count  # записываем значение по ключу
     return print(dict_counter)
